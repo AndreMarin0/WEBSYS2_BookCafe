da8d8957f614fd59f7a7075be93513648c26ff07
AndreMarin0 <20211048@s.ubaguio.edu>
2023-04-12 14:57:49 +0800

    hello

--- database/migrations/2023_03_27_155412_create_publishers_table.php
+++ database/migrations/2023_03_27_155412_create_publishers_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('publishers', function (Blueprint $table) {
+            $table->id('PubID');
+            $table->timestamps();
+            $table->string('PublisherName',50);
+            $table->string('Email',50);
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('publishers');
     }
 };
